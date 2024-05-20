707d5377edb23291a274e3fb33e0b316dca9afd6
ykkssyaa <mr.ender.03@mail.ru>
2024-05-20 18:12:52 +0300

    start server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/ykkssyaa/Posts_Service/graph"
 	"github.com/ykkssyaa/Posts_Service/internal/config"
+	"github.com/ykkssyaa/Posts_Service/internal/server/graphql"
+	"net/http"
 	"os"
 )
 import lg "github.com/ykkssyaa/Posts_Service/pkg/logger"
@@ -19,5 +24,15 @@ func main() {
 	if err := config.InitConfig(envFile); err != nil {
 		logger.Err.Fatalf(err.Error())
 	}
+
+	port := os.Getenv("PORT")
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graphql.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	logger.Info.Printf("Connect to http://localhost:%s/ for GraphQL playground", port)
+	logger.Err.Fatal(http.ListenAndServe(":"+port, nil))
 
 }
