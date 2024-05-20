2f85e300b347beb8a36413f1cbf832383d5ea74c
ykkssyaa <mr.ender.03@mail.ru>
2024-05-20 19:36:18 +0300

    creating layers in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/lib/pq"
 	"github.com/ykkssyaa/Posts_Service/graph"
 	"github.com/ykkssyaa/Posts_Service/internal/config"
+	"github.com/ykkssyaa/Posts_Service/internal/db"
+	"github.com/ykkssyaa/Posts_Service/internal/gateway"
+	"github.com/ykkssyaa/Posts_Service/internal/gateway/postgres"
 	"github.com/ykkssyaa/Posts_Service/internal/server/graphql"
+	"github.com/ykkssyaa/Posts_Service/internal/service"
 	"net/http"
 	"os"
 )
@@ -26,9 +31,44 @@ func main() {
 		logger.Err.Fatalf(err.Error())
 	}
 
-	port := os.Getenv("PORT")
+	logger.Info.Print("Connecting to Postgres.")
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graphql.Resolver{}}))
+	options := db.PostgresOptions{
+		Name:     os.Getenv("POSTGRES_DBNAME"),
+		Port:     os.Getenv("POSTGRES_PORT"),
+		Host:     os.Getenv("POSTGRES_HOST"),
+		User:     os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+	}
+
+	logger.Info.Print(options)
+	postgresDb, err := db.NewPostgresDB(options)
+
+	if err != nil {
+		logger.Err.Fatalf(err.Error())
+	}
+
+	var gateways *gateway.Gateways
+
+	logger.Info.Print("Creating Gateways.")
+	logger.Info.Print("USE_IN_MEMORY = ", os.Getenv("USE_IN_MEMORY"))
+	if os.Getenv("USE_IN_MEMORY") == "true" {
+		// TODO: use In-memory storage
+	} else {
+		posts := postgres.NewPostsPostgres(postgresDb)
+		comments := postgres.NewCommentsPostgres(postgresDb)
+		gateways = gateway.NewGateways(posts, comments)
+	}
+
+	logger.Info.Print("Creating Services.")
+	services := service.NewServices(gateways)
+
+	logger.Info.Print("Creating graphql server.")
+	port := os.Getenv("PORT")
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graphql.Resolver{
+		Posts:    services.Posts,
+		Comments: services.Comments,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
